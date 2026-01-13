efa7ed85ad83c25d1406e03ca89e183f607790db
Tim Wright <timwright12@gmail.com>
2026-01-13 13:51:56 -0500

    Adding type definitions

---
+++ types/declarations.d.ts
@@ -0,0 +1,17 @@
+declare module "eslint-plugin-lit-a11y" {
+  import type { ESLint } from "eslint";
+  const plugin: ESLint.Plugin;
+  export default plugin;
+}
+
+declare module "eslint-plugin-wc" {
+  import type { ESLint } from "eslint";
+  const plugin: ESLint.Plugin;
+  export default plugin;
+}
+
+declare module "eslint-plugin-lit" {
+  import type { ESLint } from "eslint";
+  const plugin: ESLint.Plugin;
+  export default plugin;
+}
