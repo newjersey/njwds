34744a2bb7334444f5304e12ac6e37f101f12564
Tim Wright <timwright12@gmail.com>
2026-02-11 16:05:01 -0500

    removing some unused files

--- .storybook/vitest.setup.ts
+++
@@ -1,9 +0,0 @@
-import "@testing-library/jest-dom";
-
-import * as a11yAddonAnnotations from "@storybook/addon-a11y/preview";
-import { setProjectAnnotations } from "@storybook/web-components-vite";
-import * as projectAnnotations from "./preview";
-
-// Apply Storybook configuration to Vitest
-// https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
-setProjectAnnotations([a11yAddonAnnotations, projectAnnotations]);
