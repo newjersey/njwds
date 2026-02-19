6e3cf5787c2f419c45e68a56ac7bfde5c3ce8929
Tim Wright <timwright12@gmail.com>
2026-02-19 14:27:33 -0500

    Refactor button tests

--- src/tests/button/visual/button.visual.spec.ts
+++ src/tests/button/visual/button.visual.spec.ts
@@ -1,35 +1,61 @@
 import { test, expect } from "@playwright/test";
 
 const BASE_URL = "http://localhost:6006";
-const PAGE_URL = `${BASE_URL}/iframe.html?id=elements-button--primary&viewMode=story`;
 
-const viewports = [
-  { name: "narrow", width: 375, height: 800 },
-  { name: "wide", width: 1280, height: 800 },
+// Define all the story URLs and friendly names for reporting
+const TEST_CASES = [
+  {
+    name: "default",
+    url: `${BASE_URL}/iframe.html?id=elements-button--primary&viewMode=story`,
+  },
+  {
+    name: "dark",
+    url: `${BASE_URL}/iframe.html?id=elements-button--primary&viewMode=story&args=theme%3Adark`,
+  },
+  {
+    name: "danger",
+    url: `${BASE_URL}/iframe.html?id=elements-button--primary&viewMode=story&args=theme%3Adanger`,
+  },
+  {
+    name: "secondary - dark",
+    url: `${BASE_URL}/iframe.html?id=elements-button--secondary&viewMode=story&args=theme%3Adark`,
+  },
+  {
+    name: "secondary - danger",
+    url: `${BASE_URL}/iframe.html?id=elements-button--secondary&viewMode=story&args=theme%3Adanger`,
+  },
+  {
+    name: "secondary",
+    url: `${BASE_URL}/iframe.html?id=elements-button--secondary&viewMode=story`,
+  },
+  {
+    name: "tertiary - dark",
+    url: `${BASE_URL}/iframe.html?id=elements-button--tertiary&viewMode=story&args=theme%3Adark`,
+  },
+  {
+    name: "tertiary - danger",
+    url: `${BASE_URL}/iframe.html?id=elements-button--tertiary&viewMode=story&args=theme%3Adanger`,
+  },
+  {
+    name: "tertiary",
+    url: `${BASE_URL}/iframe.html?id=elements-button--tertiary&viewMode=story`,
+  },
 ];
 
-test.describe("Button, primary - visual regression", () => {
-  for (const viewport of viewports) {
-    test(`renders correctly (${viewport.name})`, async ({ page }) => {
-      await page.setViewportSize({
-        width: viewport.width,
-        height: viewport.height,
-      });
-
+for (const { name, url } of TEST_CASES) {
+  test.describe.parallel(`Button - ${name}`, () => {
+    test(`renders correctly (${name})`, async ({ page }) => {
       // Stabilize rendering:
       await page.emulateMedia({ reducedMotion: "reduce" });
 
-      // Open the page at the URL defined above.
-      // Using `networkidle` ensures that all network requests (CSS, fonts, JS) have
-      // finished before running the test.
-      await page.goto(PAGE_URL);
+      await page.goto(url);
       await page.waitForLoadState("networkidle");
 
       // Full-page screenshot
-      await expect(page).toHaveScreenshot(`button-${viewport.name}.png`, {
+      await expect(page).toHaveScreenshot(`button-${name}.png`, {
         fullPage: true,
         maxDiffPixelRatio: 0.01, // allow a 1px difference
       });
     });
-  }
-});
+  });
+}
