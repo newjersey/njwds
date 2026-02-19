8efa5b55bf867892266af700d4c33071601303d0
Tim Wright <timwright12@gmail.com>
2026-02-19 13:16:09 -0500

    Add Alerts

--- src/stories/components/Alert/Alert.stories.ts
+++ src/stories/components/Alert/Alert.stories.ts
@@ -28,11 +28,55 @@ type Story = StoryObj<AlertProps>;
 
 export const Basic: Story = {
   args: {
-    heading: "alert heading",
-    text: "alert text",
+    heading: "Alert heading",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
     type: "default",
     slim: false,
     icon: false,
-    header: false,
+    header: true,
+  },
+};
+
+export const Info: Story = {
+  args: {
+    heading: "Informative status",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+    type: "info",
+    slim: false,
+    icon: false,
+    header: true,
+  },
+};
+
+export const Success: Story = {
+  args: {
+    heading: "Success status",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+    type: "success",
+    slim: false,
+    icon: false,
+    header: true,
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    heading: "Warning status",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+    type: "warning",
+    slim: false,
+    icon: false,
+    header: true,
+  },
+};
+
+export const Error: Story = {
+  args: {
+    heading: "Error status",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+    type: "error",
+    slim: false,
+    icon: false,
+    header: true,
   },
 };
--- src/stories/components/Alert/Alert.ts
+++ src/stories/components/Alert/Alert.ts
@@ -10,9 +10,21 @@ export interface AlertProps {
 }
 
 export const Alert = ({ heading, text, type, header, slim, icon }: AlertProps) => {
-  return html` <div class="usa-alert" role="alert">
+  return html` <div
+    class=${[
+      "usa-alert",
+      type !== "default" && `usa-alert--${type}`,
+      slim && "usa-alert--slim",
+      !icon && "usa-alert--no-icon",
+    ]
+      .filter(Boolean)
+      .join(" ")}
+    class="usa-alert"
+    role="alert"
+  >
     <div class="usa-alert__body">
-      <h3 class="usa-alert__heading">${heading}</h3>
+      ${header ? html` <h3 class="usa-alert__heading">${heading}</h3> ` : null}
+
       <p class="usa-alert__text">${text}</p>
     </div>
   </div>`;
