070603e9763ed18ca766a30a77da991a0b725cba
Tim Wright <timwright12@gmail.com>
2026-04-02 14:25:27 -0400

    Migrate graphic list pattern

---
+++ src/stories/patterns/GraphicList/GraphicList.ts
@@ -0,0 +1,33 @@
+import { html } from "lit";
+
+export const GraphicList = () => {
+  const GraphicListSection = html`
+    <div class="usa-media-block tablet:grid-col">
+      <img
+        class="usa-media-block__img"
+        src="../../public/dist/img/circle-124.png"
+        alt="placeholder image"
+      />
+      <div class="usa-media-block__body">
+        <h2 class="usa-graphic-list__heading">Graphic headings can vary.</h2>
+        <p>
+          Graphic headings can be used a few different ways, depending on what your landing page is
+          for. Highlight your values, specific program areas, or results.
+        </p>
+      </div>
+    </div>
+  `;
+
+  return html`
+    <section class="usa-graphic-list usa-section usa-section--dark">
+      <div class="grid-container">
+        <div class="usa-graphic-list__row grid-row grid-gap">
+          ${GraphicListSection} ${GraphicListSection}
+        </div>
+        <div class="usa-graphic-list__row grid-row grid-gap">
+          ${GraphicListSection} ${GraphicListSection}
+        </div>
+      </div>
+    </section>
+  `;
+};
