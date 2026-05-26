3615beb784f2e784f26d4fdf71e89804a531bc16
Tim Wright <timwright12@gmail.com>
2026-05-26 16:31:35 -0400

    Add states to name  pattern

--- src/stories/patterns/Name/Name.stories.ts
+++ src/stories/patterns/Name/Name.stories.ts
@@ -1,14 +1,20 @@
 import type { Meta, StoryObj } from "@storybook/web-components-vite";
 
-import { Name } from "./Name";
+import { Name, type NameProps } from "./Name";
 
 const meta = {
   title: "Patterns/Name",
   tags: ["autodocs"],
-  render: () => Name(),
-} satisfies Meta;
+  render: (args) => Name(args),
+} satisfies Meta<NameProps>;
 
 export default meta;
 type Story = StoryObj;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    required: false,
+    helperText: true,
+    error: false,
+  },
+};
