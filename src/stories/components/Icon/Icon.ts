2860ea98391af1fa73f8c82285e60d24e83b9a0b
Tim Wright <timwright12@gmail.com>
2026-05-27 14:01:26 -0400

    Rework Icon story

--- src/stories/components/Icon/Icon.ts
+++ src/stories/components/Icon/Icon.ts
@@ -5,27 +5,20 @@ export interface IconProps {
 }
 
 export const Icon = ({ size = 1 }: IconProps) => {
-  const iconSizeMapping = size - 1;
-
-  const iconClassList = [
-    "nj-icon--size-scale",
-    "",
-    "usa-icon--size-3",
-    "usa-icon--size-4",
-    "usa-icon--size-5",
-    "usa-icon--size-6",
-    "usa-icon--size-7",
-    "usa-icon--size-8",
-    "usa-icon--size-9",
-  ];
+  const iconSizeClasses: Record<number, string> = {
+    1: "nj-icon--size-scale",
+    2: "",
+    3: "usa-icon--size-3",
+    4: "usa-icon--size-4",
+    5: "usa-icon--size-5",
+    6: "usa-icon--size-6",
+    7: "usa-icon--size-7",
+    8: "usa-icon--size-8",
+    9: "usa-icon--size-9",
+  };
 
   return html`
-    <svg
-      class="usa-icon ${iconClassList[iconSizeMapping]}"
-      focusable="false"
-      aria-hidden="true"
-      role="img"
-    >
+    <svg class="usa-icon ${iconSizeClasses[size]}" focusable="false" aria-hidden="true" role="img">
       <use xlink:href="./img/sprite.svg#accessibility_new"></use>
     </svg>
   `;
