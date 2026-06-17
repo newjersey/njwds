2cbfb40402561b92e85b566c9dd802e21961aadf
Tim Wright <timwright12@gmail.com>
2026-06-17 09:36:45 -0400

    Add visual test assets

--- src/stories/components/Tooltip/Tooltip.ts
+++ src/stories/components/Tooltip/Tooltip.ts
@@ -7,41 +7,51 @@ export interface TooltipProps {
 
 export const Tooltip = () => {
   return html`
-    <div class="padding-8 text-center">
-      <div class="margin-4">
-        <button
-          type="button"
-          class="usa-button usa-tooltip"
-          data-position="left"
-          title="Tooltip content"
-        >
-          Show on left
-        </button>
-        <button
-          type="button"
-          class="usa-button usa-tooltip"
-          data-position="top"
-          title="Tooltip content"
-        >
-          Show on top
-        </button>
-
-        <button
-          type="button"
-          class="usa-button usa-tooltip"
-          data-position="bottom"
-          title="Tooltip content"
-        >
-          Show on bottom
-        </button>
-        <button
-          type="button"
-          class="usa-button usa-tooltip"
-          data-position="right"
-          title="Tooltip content"
-        >
-          Show on right
-        </button>
+    <div class="grid-row padding-8">
+      <div class="tablet:grid-col-3 margin-top-2 text-center">
+          <button
+            type="button"
+            class="usa-button usa-tooltip"
+            data-position="right"
+            data-classes="width-full tablet:width-auto"
+            title="Tooltip content"
+          >
+            Show on right
+          </button>
+        </div>
+        <div class="tablet:grid-col-3 margin-top-2 text-center">
+          <button
+            type="button"
+            class="usa-button usa-tooltip"
+            data-position="top"
+            data-classes="width-full tablet:width-auto"
+            title="Tooltip content"
+          >
+            Show on top
+          </button>
+        </div>
+        <div class="tablet:grid-col-3 margin-top-2 text-center">
+          <button
+            type="button"
+            class="usa-button usa-tooltip"
+            data-position="bottom"
+            data-classes="width-full tablet:width-auto"
+            title="Tooltip content"
+          >
+            Show on bottom
+          </button>
+        </div>
+        <div class="tablet:grid-col-3 margin-top-2 text-center">
+          <button
+            type="button"
+            class="usa-button usa-tooltip"
+            data-position="left"
+            data-classes="width-full tablet:width-auto"
+            title="Tooltip content"
+          >
+            Show on left
+          </button>
+        </div>
       </div>
     </div>
   `;
