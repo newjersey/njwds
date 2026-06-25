6aa797b780cad526a866acfc3692e07b85934e7f
Tim Wright <timwright12@gmail.com>
2026-06-25 11:02:17 -0400

    meeting save

--- src/stories/components/GraphicList/GraphicList.ts
+++ src/stories/components/GraphicList/GraphicList.ts
@@ -1,11 +1,24 @@
 import { html } from "lit";
 
-export const GraphicList = () => {
+type Mode = "light" | "dark";
+
+export interface GraphicListProps {
+  mode: Mode;
+}
+
+export const GraphicList = ({ mode = "dark" }: GraphicListProps) => {
+  // The "usa-section--light" class have a gray background and we're using white/default in our recommendation.
+  const isDarkTheme = mode === "dark" ? "usa-section--dark" : "";
+
   const GraphicListSection = html`
     <div class="usa-media-block tablet:grid-col">
-      <img class="usa-media-block__img" src="./img/circle-124.png" alt="placeholder image" />
+      <img
+        class="usa-media-block__img"
+        src="./img/graphic-list.svg"
+        alt="placeholder image: blue globe with a left-facing shadow"
+      />
       <div class="usa-media-block__body">
-        <h2 class="usa-graphic-list__heading">Graphic headings can vary</h2>
+        <h2 class="usa-graphic-list__heading">{heading}</h2>
         <p>
           Graphic headings can be used a few different ways, depending on what your landing page is
           for. Highlight your values, specific program areas, or results.
@@ -15,7 +28,7 @@ export const GraphicList = () => {
   `;
 
   return html`
-    <section class="usa-graphic-list usa-section usa-section--dark">
+    <section class="usa-graphic-list usa-section ${isDarkTheme}">
       <div class="grid-container">
         <div class="usa-graphic-list__row grid-row grid-gap">
           ${GraphicListSection} ${GraphicListSection}
