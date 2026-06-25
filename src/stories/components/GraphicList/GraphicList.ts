69a65eb5d72ae3c3169a962da3519d98c1c67bfc
Tim Wright <timwright12@gmail.com>
2026-06-25 16:35:51 -0400

    update graphic list to be fully responsive

--- src/stories/components/GraphicList/GraphicList.ts
+++ src/stories/components/GraphicList/GraphicList.ts
@@ -12,19 +12,26 @@ export const GraphicList = ({ mode = "dark" }: GraphicListProps) => {
 
   const GraphicListSection = html`
     <div class="usa-media-block tablet:grid-col">
-      <img
-        class="usa-media-block__img"
-        src="./img/graphic-list.svg"
-        alt="placeholder image: blue globe with a left-facing shadow"
-      />
-      <div class="usa-media-block__body">
-        <h2 class="usa-graphic-list__heading">{heading}</h2>
-        <p>
-          Graphic headings can be used a few different ways, depending on what your landing page is
-          for. Highlight your values, specific program areas, or results.
-        </p>
+      <div class="grid-row">
+        <div class="tablet:grid-col-6 padding-x-1 padding-bottom-2">
+          <img
+            class="usa-media-block__img"
+            src="./img/graphic-list.svg"
+            alt="placeholder image: blue globe with a left-facing shadow"
+          />
+        </div>
+
+        <div class="tablet:grid-col-6">
+          <h2 class="usa-graphic-list__heading">{heading}</h2>
+          <p>
+            Graphic headings can be used a few different ways, depending on what your landing page
+            is for. Highlight your values, specific program areas, or results.
+          </p>
+        </div>
       </div>
+      <!--/.grid-row-->
     </div>
+    <!--/.tablet:grid-col-->
   `;
 
   return html`
