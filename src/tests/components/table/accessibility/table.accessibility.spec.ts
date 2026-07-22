5f1cc6f056186b4ef44c77ba9e6fb2895acd1b87
Tim Wright <timwright12@gmail.com>
2026-07-22 14:59:30 -0400

    Added tests for table components

--- src/tests/components/table/accessibility/table.accessibility.spec.ts
+++ src/tests/components/table/accessibility/table.accessibility.spec.ts
@@ -10,6 +10,26 @@ const TEST_CASES = [
     name: "borderless",
     url: `/iframe.html?id=components-table--default&viewMode=story&args=border:false`,
   },
+  {
+    name: "compact",
+    url: `/iframe.html?id=components-table--compact&viewMode=story`,
+  },
+  {
+    name: "scrollable",
+    url: `/iframe.html?id=components-table--scrollable&viewMode=story`,
+  },
+  {
+    name: "sortable",
+    url: `/iframe.html?id=components-table--sortable&viewMode=story`,
+  },
+  {
+    name: "striped",
+    url: `/iframe.html?id=components-table--striped&viewMode=story`,
+  },
+  {
+    name: "responsive",
+    url: `/iframe.html?id=components-table--responsive&viewMode=story`,
+  },
 ];
 
 runA11ySuite({
--- src/tests/components/table/visual/table.visual.spec.ts
+++ src/tests/components/table/visual/table.visual.spec.ts
@@ -1,4 +1,11 @@
 import { runVisualSuite } from "../../../../utils/runVisualSuite";
+import { DEFAULT_VIEWPORT, NARROW_VIEWPORT } from "../../../../utils/config";
+
+// This component is tested over multiple viewports
+const viewports = [
+  { name: "narrow", ...NARROW_VIEWPORT },
+  { name: "wide", ...DEFAULT_VIEWPORT },
+];
 
 // Define all the story URLs and friendly names for reporting
 const TEST_CASES = [
@@ -10,9 +17,30 @@ const TEST_CASES = [
     name: "borderless",
     url: `/iframe.html?id=components-table--default&viewMode=story&args=border:false`,
   },
+  {
+    name: "compact",
+    url: `/iframe.html?id=components-table--compact&viewMode=story`,
+  },
+  {
+    name: "scrollable",
+    url: `/iframe.html?id=components-table--scrollable&viewMode=story`,
+  },
+  {
+    name: "sortable",
+    url: `/iframe.html?id=components-table--sortable&viewMode=story`,
+  },
+  {
+    name: "striped",
+    url: `/iframe.html?id=components-table--striped&viewMode=story`,
+  },
+  {
+    name: "responsive",
+    url: `/iframe.html?id=components-table--responsive&viewMode=story`,
+  },
 ];
 
 runVisualSuite({
   suiteName: "Table",
   cases: TEST_CASES,
+  viewport: viewports,
 });
