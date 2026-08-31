abdae044857a018ab92973635f1199b233c8f004
Tim Wright <timwright12@gmail.com>
2026-08-31 16:52:55 -0400

    Add color stub

--- packages/tokens/config/outputs.js
+++ packages/tokens/config/outputs.js
@@ -3,6 +3,10 @@ export const outputs = [
     name: "typography",
     filter: (token) => token.filePath && token.filePath.includes("tokens/typography/"),
   },
+  {
+    name: "color",
+    filter: (token) => token.filePath && token.filePath.includes("tokens/color/"),
+  },
 ];
 
 export default outputs;
