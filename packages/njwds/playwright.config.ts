65e24fbc18aa592b3f5848d8689d27ac4d8b77aa
Tim Wright <timwright12@gmail.com>
2026-09-10 16:09:01 -0400

    clean up playwright implementation

--- packages/njwds/playwright.config.ts
+++ packages/njwds/playwright.config.ts
@@ -1,6 +1,6 @@
 // playwright.config.ts
 import { defineConfig } from "@playwright/test";
-import { DEFAULT_VIEWPORT } from "./src/utils/config";
+import { DEFAULT_VIEWPORT, STORYBOOK_URL } from "./src/utils/config";
 
 export default defineConfig({
   testDir: "./src/tests",
@@ -8,6 +8,14 @@ export default defineConfig({
   snapshotPathTemplate: "{testDir}/__screenshots__/{testFilePath}/{arg}{ext}",
   use: {
     viewport: DEFAULT_VIEWPORT,
+  },
+  webServer: {
+    command: "npx http-server storybook-static -p 6006",
+    url: STORYBOOK_URL,
+    reuseExistingServer: !process.env.CI,
+    timeout: 30_000,
+    stdout: "ignore",
+    stderr: "pipe",
   },
   projects: [
     {
