db3818e5ef613a0a36740a098e42c5c747cf02c7
Colin Buyck <colinbuyck@mightyacorn.com>
2026-09-15 11:58:31 -0700

    feat: dismissable alert component

--- packages/njwds/src/stories/components/Alert/Alert.ts
+++ packages/njwds/src/stories/components/Alert/Alert.ts
@@ -7,9 +7,10 @@ export interface AlertProps {
   header: boolean;
   slim: boolean;
   icon: boolean;
+  dismissable: boolean;
 }
 
-export const Alert = ({ heading, text, type, header, slim, icon }: AlertProps) => {
+export const Alert = ({ heading, text, type, header, slim, icon, dismissable }: AlertProps) => {
   // When slim is true, header must be false
   const showHeader = slim ? false : header;
 
@@ -19,6 +20,7 @@ export const Alert = ({ heading, text, type, header, slim, icon }: AlertProps) =
       type !== "default" && `usa-alert--${type}`,
       slim && "usa-alert--slim",
       !icon && "usa-alert--no-icon",
+      dismissable && "usa-alert--dismissable",
     ]
       .filter(Boolean)
       .join(" ")}
@@ -28,6 +30,26 @@ export const Alert = ({ heading, text, type, header, slim, icon }: AlertProps) =
       ${showHeader ? html` <h3 class="usa-alert__heading">${heading}</h3> ` : null}
 
       <p class="usa-alert__text">${text}</p>
+
+      ${dismissable
+        ? html`
+            <button
+              type="button"
+              class="usa-alert__close"
+              aria-label="Close this alert"
+              data-close-alert
+            >
+              <svg
+                class="usa-icon usa-icon--size-3"
+                aria-hidden="true"
+                focusable="false"
+                role="img"
+              >
+                <use href="./img/sprite.svg#close"></use>
+              </svg>
+            </button>
+          `
+        : null}
     </div>
   </div>`;
 };
